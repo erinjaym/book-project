ec7a41011f92cfd1f63470f2c1e20848823a1089
erinjaym <erin.jay.moyer@gmail.com>
2021-10-18 20:29:54 -0400

    Seperated JavaScript into its own file. Organized stylesheet. Created template for extras page. Next Step: Create picture gallery for extras page and clean up code for deployment on Digital Ocean.

---
+++ bookpage.js
@@ -0,0 +1,47 @@
+// current download selections
+function downloadChoice() {
+    let userChoice = document.getElementById("choice").value;
+    if (userChoice ==="EARHT"){
+        console.log('wHY');
+        window.open("./books/EARHT.pdf", "Even A Rose Has Thorns",);
+    }else if (userChoice === "BL"){
+        window.open("./books/BL.pdf", "Black Lotus",);
+    }else if (userChoice === "19"){
+        window.open("./books/19AHR.pdf", "Even A Rose Has Thorns",);
+    }
+}
+
+// dropdown menu
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".menu-button");
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+    } else {
+        menu.classList.add("showMenu");
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+    function(menuItem) { 
+        menuItem.addEventListener("click", toggleMenu);
+    }
+)
+
+// hamburger animation 
+const menuButton = document.querySelector('.menu-button');
+let menuOpen = false;
+
+menuButton.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuButton.classList.add('open');
+        menuOpen = true;
+    }else {
+        menuButton.classList.remove('open');
+        menuOpen = false;
+    }
+});
